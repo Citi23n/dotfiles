afaa2736d66643bdbfead93742c0d0a2b8616d40
Zane <deni23n@gmail.com>
2024-04-01 13:18:03 +0200

    changes

--- dot_scripts/executable_rofi-playlist.sh
+++ dot_scripts/executable_rofi-playlist.sh
@@ -15,19 +15,16 @@ dalist["Ansible"]=https://www.youtube.com/playlist?list=PLT98CRl2KxKEUHie1m24-wk
 dalist["Linux Basics sysadmin"]=https://www.youtube.com/playlist?list=PLtK75qxsQaMLZSo7KL-PmiRarU7hrpnwK
 dalist["JS Data Structures"]=https://www.youtube.com/playlist?list=PLC3y8-rFHvwjPxNAKvZpdnsr41E0fCMMP
 
-# mpv --ytdl-format='bestvideo[height<=?720]+bestaudio[abr<=128]/best[height<=?720]' ${dalist[$selected_key]} &
-
 # Option1: Use fzf to select a key
-# selected_key=$(printf '%s\n' "${!dalist[@]}" | fzf --prompt="pick playlist>" -e -i)
+#selected_key=$(printf '%s\n' "${!dalist[@]}" | fzf --prompt="pick playlist>" -e -i)
 # Option2: Use Rofi to select a key
 selected_key=$(printf '%s\n' "${!dalist[@]}" | rofi -dmenu -p "Select a command:")
-echo Now playing $selected_key
-#Check if a key was selected
+
+#  Check if a key was selected
 if [ -n "$selected_key" ]; then
+    # play corresponding value  through mpv
     mpv --ytdl-format='bestvideo[height<=?720]+bestaudio[abr<=128]/best[height<=?720]' ${dalist[$selected_key]} &
-    #selected_command=${dalist[$selected_key]}
-    # Run the selected command
-    #eval "$selected_command"
+    echo Now playing $selected_key
 else
     echo "No command selected."
 fi
