f6914570d14bc13b9a8b1df6f904a23f6c333b43
Zane <deni23n@gmail.com>
2024-04-05 21:27:23 +0200

    changes

--- dot_scripts/executable_rofi-playlist.sh
+++ dot_scripts/executable_rofi-playlist.sh
@@ -25,7 +25,7 @@ if [ -n "$selected_key" ]; then
     # play corresponding value  through mpv
     mpv --ytdl-format='bestvideo[height<=?720]+bestaudio[abr<=128]/best[height<=?720]' ${dalist[$selected_key]} &
     #echo Now playing $selected_key
-    notify-send "Now loading ${!dalist[$selected_key]}" --icon=youtube --app-name="Playlist"
+    notify-send "Now loading ${dalist[$selected_key]}" --icon=youtube --app-name="Playlist"
 else
     echo "No command selected."
 fi
