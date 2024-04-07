6c1d7aa779006a9f475a10edc1f658c6ab8c48c0
Zane <deni23n@gmail.com>
2024-04-07 11:21:38 +0200

    changes

--- dot_scripts/executable_rofi-playlist.sh
+++ dot_scripts/executable_rofi-playlist.sh
@@ -6,7 +6,7 @@ dalist["AAA Stuff"]=https://www.youtube.com/playlist?list=PLw0ioCj5tQV0nYBO1YJdc
 dalist["AAA Learn"]=https://www.youtube.com/playlist?list=PLw0ioCj5tQV2uy0l4H0C-DAO15X1x5YrQ
 dalist["Linux Stuff"]=https://www.youtube.com/playlist?list=PLw0ioCj5tQV2dFuI5rL7V4NRu7lE8O9Qd
 dalist["VIM"]=https://www.youtube.com/playlist?list=PLw0ioCj5tQV0NVOovOiIoUnMNveamfcJV
-dalist[".Long talks"]=https://youtube.com/playlist?list=PLw0ioCj5tQV3FsxZkFrGf3NR1EEYNNd0v&si=z3lOwY1PX4zsUfUW
+dalist[".Long talks"]="https://youtube.com/playlist?list=PLw0ioCj5tQV3FsxZkFrGf3NR1EEYNNd0v&si=z3lOwY1PX4zsUfUW"
 dalist["Tunes"]=https://www.youtube.com/playlist?list=PLw0ioCj5tQV2ULk1g-PLWesjddSkVOGEB
 dalist["Food"]=https://www.youtube.com/playlist?list=PLw0ioCj5tQV0ESuxkDip4lomBoqsZIIJf
 dalist["SAO Abridged"]=https://www.youtube.com/playlist?list=PLuAOJfsMefuej06Q3n4QrSSC7qYjQ-FlU
@@ -14,7 +14,7 @@ dalist["XFCE Customization"]=https://www.youtube.com/playlist?list=PLw0ioCj5tQV3
 dalist["Ansible"]=https://www.youtube.com/playlist?list=PLT98CRl2KxKEUHie1m24-wkyHpEsa4Y70
 dalist["Linux Basics sysadmin"]=https://www.youtube.com/playlist?list=PLtK75qxsQaMLZSo7KL-PmiRarU7hrpnwK
 dalist["JS Data Structures"]=https://www.youtube.com/playlist?list=PLC3y8-rFHvwjPxNAKvZpdnsr41E0fCMMP
-
+dalist["DT QTile"]="https://youtube.com/playlist?list=PL5--8gKSku15-C4mBKRpQVcaat4zwe4Gu&si=O9t6MEhTevzpGVkH"
 # Option1: Use fzf to select a key
 #selected_key=$(printf '%s\n' "${!dalist[@]}" | fzf --prompt="pick playlist>" -e -i)
 # Option2: Use Rofi to select a key
